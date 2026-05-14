30241740b215bba1c0b08006d0ffd518839d7dc3
BernhardFocke <b.focke@students.htl-leonding.ca.at>
2026-05-14 18:45:09 +0200

    overall Bug fix, musicMixer dev

--- project/backend/sides/musicMixer.php
+++ project/backend/sides/musicMixer.php
@@ -1,11 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Music Mixer</title>
+
+    <link rel="stylesheet" href="../css/musicMixerStyle.css">
 </head>
+
 <body>
-    
+    <nav>
+        <div id="navBar">
+            <div class="navItem">
+                <img src="../../frontend/img/startpage/logoWhite.png" alt="whiteLogo">
+            </div>
+
+            <div class="navItem">
+                <p>Music Mixer</p>
+            </div>
+
+            <div class="navItem">
+                <p>Placeholder</p>
+            </div>
+
+            <div class="navItem">
+                <p>Placeholder</p>
+            </div>
+
+            <div class="navItem" id="login">
+                <p>Login</p>
+            </div>
+        </div>
+    </nav>
+
+
+    <div id="mixer">
+        
+    </div>
+
 </body>
+
 </html>
